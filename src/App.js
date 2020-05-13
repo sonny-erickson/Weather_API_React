92b453ec33c118aa9fad2fc812163feb24297431
Sonny <pet63910@hotmail.fr>
2020-05-13 10:22:47 +0200

    Download and use styled-components
    Create Css in JS, create container and wrapper with good height and width.
    Test font awesome.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Wrapper = styled.div`
+  height: 70%;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.25em 1em;
+  background-color: rgba(234, 21, 24, 0.7);
+`;
+
+const ContainerComponents = styled.div`
+  height: 70%;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: rgba(3, 169, 24, 0.7);
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Wrapper>
+          <ContainerComponents>
+          
+            <FontAwesomeIcon icon={faBolt} color="blue" />
+          </ContainerComponents>
+        </Wrapper>
+      </div>
+    );
+  }
 }
-
 export default App;
